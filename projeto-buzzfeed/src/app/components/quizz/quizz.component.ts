21dee6dbfb1cb61ed119ea2c99517d8a4eff58a3
Lucas Sousa <111785664+LucasSousa15@users.noreply.github.com>
2024-12-23 21:49:15 -0300

    Project finished

--- projeto-buzzfeed/src/app/components/quizz/quizz.component.ts
+++ projeto-buzzfeed/src/app/components/quizz/quizz.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import quizz__questions from '../../assets/data/quizz_questions.json'
+
 
 @Component({
   selector: 'app-quizz',
@@ -22,5 +24,59 @@ export class QuizzComponent {
 
   finished:boolean = false;
 
+  constructor () {}
+
+  ngOnInit(): void {
+    if(quizz__questions) {
+      this.finished = false
+      this.title = quizz__questions.title
+
+      this.questions = quizz__questions.questions
+      this.questionSelected = this.questions[this.questionIndex]
+
+      this.questionIndex = 0
+      this.questionMaxInder = this.questions.length
+      console.log(this.questionIndex)
+      console.log(this.questionMaxInder)
+    }
+  }
+
+  playerChoice(value:string) {
+    this.answers.push(value)
+    this.nextStep()
+    console.log(this.answers)
+  }
+
+  async nextStep (){
+    this.questionIndex+=1;
+
+    if (this.questionMaxInder > this.questionIndex){
+      this.questionSelected = this.questions[this.questionIndex]
+    }
+
+    else{
+      const finalAnswer: string = await this.checkResult(this.answers)
+      this.finished = true
+      this.answerSelected = quizz__questions.results[finalAnswer as keyof typeof quizz__questions.results]
+      console.log(this.answerSelected)
+    }
+  }
+
+  async checkResult (answers:string[]) {
+    const result = answers.reduce((previous, current, i, arr)=> {
+      if (
+        arr.filter(item => item === previous).length > 
+        arr.filter(item => item === current).length 
+      ) {
+        return previous
+      }
+
+      else {
+        return current}
+    } )
+
+       return result
+       console.log('Aqui o' + result)
+  }
 
 }
